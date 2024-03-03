bd3f46d9a846d68c7c9f282e26440482812cb6a4
mfauzian <162025073+mfauzian@users.noreply.github.com>
2024-03-03 21:55:05 +0700

    Add files via upload

---
+++ home task soal 1.js
@@ -0,0 +1,233 @@
+//1. Buatlah 20 variable yang dipakai untuk menghitung 5 angka, outputkan hasilnya:
+// Jawaban :
+console.log('jawaban soal no. 1:')
+let value1 = 5+5+5+5+5
+console.log ('nilai dari variabel1:',value1)
+let value3 = 10+10-5-5-5
+console.log ('nilai dari variabel2:',value3)
+let value4 = 30-10-5-5-5
+console.log ('nilai dari variabel3:',value4)
+let value5 = 5*5*5*5*5
+console.log ('nilai dari variabel4:',value5)
+let value6 = 10*10/5+5-5
+console.log ('nilai dari variabel5:',value6)
+let valu7 = 20*5/5+10-5
+console.log ('nilai dari variabel6:',valu7)
+let value8 = 50+50/5-5-5
+console.log ('nilai dari variabel7:',value8)
+let value9 = 10*10*5/5-5
+console.log ('nilai dari variabel8:',value9)
+let value10 = 10+10*5-5/5
+console.log ('nilai dari variabel10:',value10)
+let value11 = 10+10+5-5-5
+console.log ('nilai dari variabel11:',value11)
+let value12 = 10/10+5-5*5
+console.log ('nilai dari variabel12:',value12)
+let value13 = 10+10+5*5-5
+console.log ('nilai dari variabel13:',value13)
+let value14 = 10*10+5-5/5
+console.log ('nilai dari variabel14:',value14)
+let value15 = 20+10+5*5/5
+console.log ('nilai dari variabel15:',value15)
+let value16 = 10+10*5-5-5
+console.log ('nilai dari variabel16:',value16)
+let value17 = 15*10/5+5-5
+console.log ('nilai dari variabel17:',value17)
+let value18 = 10+10+5-5-5
+console.log ('nilai dari variabel18:',value18)
+let value19 = 20+10*5-10/5
+console.log ('nilai dari variabel19:',value19)
+let value20 = 10/10+5*5-2
+console.log ('nilai dari variabel20:',value20)
+
+
+//2. buat masing masing 10 dari setiap operator perbandingan, outputkan hasilnya
+
+// Jawaban :
+console.log('')
+console.log('jawaban soal no. 2:')
+//Operator Perbandingan ' == ' 
+let perbandingan1 = 5 == 5
+console.log('hasil dari perbandingan nilai : 5 == 5 adalah',perbandingan1)
+let perbandingan2 = 9 == 9
+console.log('hasil dari perbandingan nilai : 9 == 9 adalah',perbandingan2)
+let perbandingan3 = 100 == 100
+console.log('hasil dari perbandingan nilai : 100 == 100 adalah',perbandingan3)
+let perbandingan4 = 1000 == 1000
+console.log('hasil dari perbandingan nilai : 000 == 1000 adalah',perbandingan4)
+let perbandingan5 = -5 == 5
+console.log('hasil dari perbandingan nilai : -5 == 5 adalah',perbandingan5)
+let perbandingan6 = -100 == -100
+console.log('hasil dari perbandingan nilai : -100 == -100 adalah',perbandingan6)
+let perbandingan7 = 200 == -200
+console.log('hasil dari perbandingan nilai : 200 == -200 adalah',perbandingan7)
+let perbandingan8 = 'putih' == 'putih'
+console.log('hasil dari perbandingan nilai : putih == putih adalah',perbandingan8)
+let perbandingan9 = 'kaya' == 'kaya'
+console.log('hasil dari perbandingan nilai :kaya == kaya adalah',perbandingan9)
+let perbandingan10 = 'saya' == 'saya'
+console.log('hasil dari perbandingan nilai : saya == saya adalah',perbandingan10)
+
+
+
+//Operator Perbandingan ' === ' 
+console.log('')
+let perbandingan11 = 5 === 5
+console.log('hasil dari perbandingan nilai : 5 === 5 adalah',perbandingan11)
+let perbandingan12 = 9 === 9
+console.log('hasil dari perbandingan nilai : 9 === 9 adalah',perbandingan12)
+let perbandingan13 = 100 === 100
+console.log('hasil dari perbandingan nilai : 100 === 100 adalah',perbandingan13)
+let perbandingan14 = '-1000' === '-1000'
+console.log('hasil dari perbandingan nilai : 1000 === 1000 adalah',perbandingan14)
+let perbandingan15 = -5 === 5
+console.log('hasil dari perbandingan nilai :"-1000" === "-1000" adalah',perbandingan15)
+let perbandingan16 = -100 === -100
+console.log('hasil dari perbandingan nilai :-100 === -100 adalah',perbandingan16)
+let perbandingan17 = 200 === '-200'
+console.log('hasil dari perbandingan nilai :200 === -"200" adalah',perbandingan17)
+let perbandingan18 = 'putih' === 'putih'
+console.log('hasil dari perbandingan nilai :putih === putih adalah',perbandingan18)
+let perbandingan19 = 'kaya' === 'kaya'
+console.log('hasil dari perbandingan nilai :kaya === kaya adalah',perbandingan19)
+let perbandingan20 = 1 === '1'
+console.log('hasil dari perbandingan nilai :1 === "1" adalah',perbandingan20)
+
+//Operator Perbandingan ' != ' 
+console.log('')
+let perbandingan21 = 2 != 3
+console.log('hasil dari perbandingan nilai : 2 != 3 adalah',perbandingan21)
+let perbandingan22 = 2 != 2
+console.log('hasil dari perbandingan nilai : 2 != 2 adalah',perbandingan22)
+let perbandingan23 = 2 != "2"
+console.log('hasil dari perbandingan nilai : 2 != "2" adalah',perbandingan23)
+let perbandingan24 = -2 != -3
+console.log('hasil dari perbandingan nilai : -2 != -3 adalah',perbandingan24)
+let perbandingan25 = -2 != -2
+console.log('hasil dari perbandingan nilai : -2 != -2 adalah',perbandingan25)
+let perbandingan26 = 2 != "-2"
+console.log('hasil dari perbandingan nilai : 2 != "-2" adalah',perbandingan26)
+let perbandingan27 = 12 != "12"
+console.log('hasil dari perbandingan nilai : 12 != "12" adalah',perbandingan27)
+let perbandingan28 = -21 != -21
+console.log('hasil dari perbandingan nilai : 21 != -21 adalah',perbandingan28)
+let perbandingan29 = -21 != "-21"
+console.log('hasil dari perbandingan nilai : -21 != "-21" adalah',perbandingan29)
+let perbandingan30 = -2 != "-2"
+console.log('hasil dari perbandingan nilai : 2 != "-2" adalah',perbandingan30)
+
+//Operator Perbandingan ' !== ' 
+console.log('')
+let perbandingan31 = 2 != 3
+console.log('hasil dari perbandingan nilai : 2 != 3 adalah',perbandingan31)
+let perbandingan32 = 2 != 2
+console.log('hasil dari perbandingan nilai : 2 != 2 adalah',perbandingan32)
+let perbandingan33 = 2 != "2"
+console.log('hasil dari perbandingan nilai : 2 != "2" adalah',perbandingan33)
+let perbandingan34 = -2 != -2
+console.log('hasil dari perbandingan nilai : -2 != -3 adalah',perbandingan34)
+let perbandingan35 = -2 != "-2"
+console.log('hasil dari perbandingan nilai : -2 != "-2" adalah',perbandingan35)
+let perbandingan36 = 2 != "-2"
+console.log('hasil dari perbandingan nilai : 2 != "-2" adalah',perbandingan36)
+let perbandingan37 = 12 != "12"
+console.log('hasil dari perbandingan nilai : 12 != "12" adalah',perbandingan37)
+let perbandingan38 = -21 != "-21"
+console.log('hasil dari perbandingan nilai : -21 != "-21" adalah',perbandingan38)
+let perbandingan39 = "-21" != "-21"
+console.log('hasil dari perbandingan nilai : "-21" != "-21" adalah',perbandingan39)
+let perbandingan40 = 45 != -45
+console.log('hasil dari perbandingan nilai : 45 != -45 adalah',perbandingan40)
+
+
+//Operator Perbandingan ' > ' 
+console.log('')
+let perbandingan41 = 15 > 10
+console.log('hasil dari perbandingan nilai : 15 > 10 adalah',perbandingan41)
+let perbandingan42 = 15 > 15
+console.log('hasil dari perbandingan nilai : 15 > 15 adalah',perbandingan42)
+let perbandingan43 = 15 > -15
+console.log('hasil dari perbandingan nilai : 15 > -15 adalah',perbandingan43)
+let perbandingan44 = -15 > -15
+console.log('hasil dari perbandingan nilai : -15 > -15 adalah',perbandingan44)
+let perbandingan45 = 15 > "10"
+console.log('hasil dari perbandingan nilai : 15 > "10" adalah',perbandingan45)
+let perbandingan46 = 15 > "15"
+console.log('hasil dari perbandingan nilai : 15 > "15" adalah',perbandingan46)
+let perbandingan47 = 15 > "-15"
+console.log('hasil dari perbandingan nilai : 15 > "-15" adalah',perbandingan47)
+let perbandingan48 =  "-15" > "-15"
+console.log('hasil dari perbandingan nilai : "-15" > "-15" adalah',perbandingan48)
+let perbandingan49 = 0.25 > 0.25
+console.log('hasil dari perbandingan nilai : 0.25 > 0.25 adalah',perbandingan49)
+let perbandingan50 = 0.25 > "0.25"
+console.log('hasil dari perbandingan nilai : 0.25 > "0.25" adalah',perbandingan50)
+
+//Operator Perbandingan ' >= ' 
+console.log('')
+let perbandingan51 = 15 >= 10
+console.log('hasil dari perbandingan nilai : 15 >= 10 adalah',perbandingan51)
+let perbandingan52 = 15 >= 15
+console.log('hasil dari perbandingan nilai : 15 >= 15 adalah',perbandingan52)
+let perbandingan53 = 15 >= -15
+console.log('hasil dari perbandingan nilai : 15 >= -15 adalah',perbandingan53)
+let perbandingan54 = -15 >= -15
+console.log('hasil dari perbandingan nilai : -15 >= -15 adalah',perbandingan54)
+let perbandingan55 = 15 >= "10"
+console.log('hasil dari perbandingan nilai : 15 >= "10" adalah',perbandingan55)
+let perbandingan56 = 15 >= "15"
+console.log('hasil dari perbandingan nilai : 15 >= "15" adalah',perbandingan56)
+let perbandingan57 = 15 >= "-15"
+console.log('hasil dari perbandingan nilai : 115 >= "-15" adalah',perbandingan57)
+let perbandingan58 =  "-15" >= "-15"
+console.log('hasil dari perbandingan nilai : "-15" > "-15" adalah',perbandingan58)
+let perbandingan59 = 0.25 >= 0.25
+console.log('hasil dari perbandingan nilai : 0.25 >= 0.25 adalah',perbandingan59)
+let perbandingan60 = 0.25 >= "0.25"
+console.log('hasil dari perbandingan nilai :0.25 >= "0.25" adalah',perbandingan60)
+
+//Operator Perbandingan ' < ' 
+console.log('')
+let perbandingan71 = 25 < 30
+console.log('hasil dari perbandingan nilai : 25 < 30 adalah',perbandingan71)
+let perbandingan72 = 30 < 30
+console.log('hasil dari perbandingan nilai : 30 < 30 adalah',perbandingan72)
+let perbandingan73 = 30 < -30
+console.log('hasil dari perbandingan nilai : 30 < -30 adalah',perbandingan73)
+let perbandingan74 = -30 < -30
+console.log('hasil dari perbandingan nilai : -30 < -30 adalah',perbandingan74)
+let perbandingan75 = 0.7 < 0.7 
+console.log('hasil dari perbandingan nilai : 0.7 < 0.7   adalah',perbandingan75)
+let perbandingan76 = 0.5 < "0.5" 
+console.log('hasil dari perbandingan nilai : 0.5 < "0.5" adalah',perbandingan76)
+let perbandingan77 = "0.5" < "0.5" 
+console.log('hasil dari perbandingan nilai : "0.5" < "0.5"  adalah',perbandingan77)
+let perbandingan78 =  -0.17 < -0.15
+console.log('hasil dari perbandingan nilai : -0.17 < -0.15 adalah',perbandingan78)
+let perbandingan79 = -0.15 < -0.15
+console.log('hasil dari perbandingan nilai : -0.15 < -0.15 adalah',perbandingan79)
+let perbandingan80 = -0.15 < "-0.15"
+console.log('hasil dari perbandingan nilai : -0.15 < "-0.15" adalah',perbandingan80)
+
+//Operator Perbandingan ' <= ' 
+console.log('')
+let perbandingan81 = 25 <= 30
+console.log('hasil dari perbandingan nilai : 25 <= 30 adalah',perbandingan81)
+let perbandingan82 = 30 <= 30
+console.log('hasil dari perbandingan nilai : 30 <= 30 adalah',perbandingan82)
+let perbandingan83 = 30 <= -30
+console.log('hasil dari perbandingan nilai : 30 <= -30 adalah',perbandingan83)
+let perbandingan84 = -30 <= -30
+console.log('hasil dari perbandingan nilai : -30 <= -30 adalah',perbandingan84)
+let perbandingan85 = 0.7 <= 0.7 
+console.log('hasil dari perbandingan nilai : 0.7 <= 0.7    adalah',perbandingan85)
+let perbandingan86 = 0.5 <= "0.5" 
+console.log('hasil dari perbandingan nilai :  0.5 <= "0.5"  adalah',perbandingan86)
+let perbandingan87 = "0.5" <= "0.5" 
+console.log('hasil dari perbandingan nilai : "0.5" <= "0.5"  adalah',perbandingan87)
+let perbandingan88 =  -0.17 <= -0.15
+console.log('hasil dari perbandingan nilai : -0.17 <= -0.15 adalah',perbandingan88)
+let perbandingan89 = -0.15 <= -0.15
+console.log('hasil dari perbandingan nilai : -0.15 <= -0.15 adalah',perbandingan89)
+let perbandingan90 = -0.15 <= "-0.15"
+console.log('hasil dari perbandingan nilai : -0.15 <= "-0.15" adalah',perbandingan90)
